75553aa1307f148dd77caaef6c7a73e010a8cfee
Tii04 <tiisetso.qsebata@gmail.com>
2023-08-31 14:47:46 +0200

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+"""a program that solves the N queens problem."""
+
+import sys
+
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column on top of the current row
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
+
+    # Check for queens in the upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    # Check for queens in the upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True
+
+
+def solve_nqueens(N):
+    def backtrack(row):
+        if row == N:
+            solutions.append(["".join(row) for row in board])
+            return
+
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+
+    solutions = []
+    board = [['.' for _ in range(N)] for _ in range(N)]
+    backtrack(0)
+
+    return solutions
+
+
+def print_solutions(solutions):
+    for solution in solutions:
+        for row in solution:
+            print("[" + ", ".join(str(int(col == 'Q')) for col in row) + "]")
+        print()
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        if N < 4:
+            raise ValueError
+    except ValueError:
+        print("N must be a number and at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(N)
+    print_solutions(solutions)
+
+
+if __name__ == "__main__":
+    main()
