f09c482d1e0f90a2633956e90f0dc4988b22e678
Tii04 <tiisetso.qsebata@gmail.com>
2023-08-31 14:57:38 +0200

    Implementing different solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,71 +1,52 @@
 #!/usr/bin/python3
-
-"""a program that solves the N queens problem."""
+"""
+Program that solves the Nqueens problem
+"""
 
 import sys
 
 
-def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column on top of the current row
-    for i in range(row):
-        if board[i][col] == 'Q':
+def is_safe(board, row, col):
+    for i in range(col):
+        if (board[i] == row or board[i] == row + col - i
+                or board[i] == row - col + i):
             return False
-
-    # Check for queens in the upper-left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 'Q':
-            return False
-
-    # Check for queens in the upper-right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == 'Q':
-            return False
-
     return True
 
 
-def solve_nqueens(N):
-    def backtrack(row):
-        if row == N:
-            solutions.append(["".join(row) for row in board])
-            return
-
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row][col] = 'Q'
-                backtrack(row + 1)
-                board[row][col] = '.'
-
+def solve_nqueens(n):
+    board = [-1] * n
     solutions = []
-    board = [['.' for _ in range(N)] for _ in range(N)]
+
+    def backtrack(col):
+        if col == n:
+            solution = [[i, board[i]] for i in range(n)]
+            solutions.append(solution)
+        else:
+            for row in range(n):
+                if is_safe(board, row, col):
+                    board[col] = row
+                    backtrack(col + 1)
+
     backtrack(0)
 
     return solutions
 
 
-def print_solutions(solutions):
-    for solution in solutions:
-        for row in solution:
-            print("[" + ", ".join(str(int(col == 'Q')) for col in row) + "]")
-        print()
-
-
-def main():
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
-        N = int(sys.argv[1])
-        if N < 4:
-            raise ValueError
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
     except ValueError:
-        print("N must be a number and at least 4")
+        print("N must be a number")
         sys.exit(1)
 
-    solutions = solve_nqueens(N)
-    print_solutions(solutions)
-
-
-if __name__ == "__main__":
-    main()
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
