155eec033005593a415f26a8e471091dfd59adc6
Tii04 <tiisetso.qsebata@gmail.com>
2023-09-07 15:49:59 +0200

    Hot fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,51 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
+const movieId = process.argv[2];
 
-// Define the base URL for the Star Wars API
-const BASE_URL = 'https://swapi.dev/api/';
+function getMovieCharacters(movieId) {
+  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
 
-// Function to fetch characters of a specific movie
-async function getMovieCharacters(movieId) {
-  try {
-    // Make a GET request to the /films/ endpoint with the provided movieId
-    const response = await axios.get(`${BASE_URL}films/${movieId}/`);
-    const filmData = response.data;
+  request.get(filmsUrl, (error, response, body) => {
+    if (response.statusCode === 200) {
+      const filmsData = JSON.parse(body);
+      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
 
-    // Extract the character URLs from the film data
-    const characterUrls = filmData.characters;
+      if (movieData) {
+        const charactersUrls = movieData.characters;
+        const characters = [];
 
-    // Iterate through character URLs and fetch character data
-    for (const characterUrl of characterUrls) {
-      const characterResponse = await axios.get(characterUrl);
-      const characterData = characterResponse.data;
-      console.log(characterData.name);
-    }
-  } catch (error) {
-    if (error.response) {
-      console.error(`Error: ${error.response.status} - ${error.response.statusText}`);
+        for (const charUrl of charactersUrls) {
+          request.get(charUrl, (error, response, body) => {
+            if (response.statusCode === 200) {
+              const characterData = JSON.parse(body);
+              characters.push(characterData.name);
+              if (characters.length === charactersUrls.length) {
+                printCharacters(characters, movieData.title);
+              }
+            } else {
+              console.log(`Error: ${response.statusCode}`);
+            }
+          });
+        }
+      } else {
+        console.log(`Movie ID ${movieId} not found.`);
+      }
     } else {
-      console.error(`An unexpected error occurred: ${error.message}`);
+      console.log(`Error: ${response.statusCode}`);
     }
+  });
+}
+
+function printCharacters(characters, movieTitle) {
+  console.log(`Characters in ${movieTitle}:`);
+  for (const character of characters) {
+    console.log(character);
   }
 }
 
-if (process.argv.length !== 3) {
-  console.log('Usage: node starWarsCharacters.js <Movie ID>');
-  process.exit(1);
+if (movieId) {
+  getMovieCharacters(movieId);
+} else {
+  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
 }
-
-const movieId = process.argv[2];
-
-// Call the function to get and print characters of the specified movie
-getMovieCharacters(movieId);
